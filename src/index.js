f8a7a8bf01a7c8255815c5fbd42972d0b48f06f1
Utrennik <utrennik@list.ru>
2021-01-28 13:27:30 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+const { sort } = require("semver");
+
+module.exports = function towelSort(matrix) {
+
+	if (!matrix || matrix.length == 0) return []; 
+
+	const result = [];
+
+	for (let i = 0; i < matrix.length; i++){
+		if (i == 0 || i % 2 == 0) {
+			matrix[i].sort((a, b) => a - b);
+		} else matrix[i].sort((a, b) => b - a);
+
+		matrix[i].forEach((el) => result.push(el));
+	}
+
+	return result;
+	
 }
